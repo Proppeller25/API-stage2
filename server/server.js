2ad88d3397cb5ddb8b4973ede48662e590b3c4ae
Emmanuel <emmanuelegbebo@gmail.com>
2026-04-10 17:20:25 +0100

    fix vercel deployment error through CLI instead

--- server/server.js
+++ server/server.js
@@ -16,6 +16,13 @@ app.get('/', (req, res) => {
 app.get('/api/classify', async (req, res) => {
   try {
     const {name} = req.query
+
+    if (!name) 
+      return res.status(400).json({status: "error", message: "Missing or empty name parameter"})
+    
+    if (typeof name !== "string")
+       return res.status(422).json({status: "error", message: "Name is not a string"})
+    
     const UTCDate = new Date().toISOString()
     const apiRes = await fetch(
       `https://api.genderize.io?name=${encodeURIComponent(name)}`,
@@ -34,8 +41,6 @@ app.get('/api/classify', async (req, res) => {
       processedAt: UTCDate 
     }
 
-    if (!name) return res.status(400).json({status: "error", message: "Missing or empty name parameter"})
-    if (typeof name !== "string") return res.status(422).json({status: "error", message: "Name is not a string"})
     
     if (!apiData.gender || !apiData.count) return res.status(404).json({status: "error", message: "No apiData or prediction available for the provided name"})
 
